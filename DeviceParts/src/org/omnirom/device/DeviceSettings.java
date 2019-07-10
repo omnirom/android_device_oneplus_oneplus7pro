c2ff2f8ebd328180519f9894a78699f30b19baa9
Marko Man <darkobas@gmail.com>
2019-07-10 11:55:52 +0200

    clean devicesettings deviceparts
    Change-Id: I75808630a8eec6addb73f8a56b95ea9056aaef2e

--- DeviceParts/src/org/omnirom/device/DeviceSettings.java
+++ DeviceParts/src/org/omnirom/device/DeviceSettings.java
@@ -54,7 +54,6 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String KEY_DCI_SWITCH = "dci";
     public static final String KEY_NIGHT_SWITCH = "night";
     public static final String KEY_WIDE_SWITCH = "wide";
-    public static final String KEY_ONEPLUS_SWITCH = "oneplus";
 
     public static final String KEY_OTG_SWITCH = "otg_switch";
 
