7ff082964cc5945b3449a00cb409bedec11865ab
Vachounet <vachounet@live.fr>
2019-09-23 12:39:07 +0200

    oneplus7pro: update lunch combos
    Change-Id: Id7ca045a7f1d5da9644e5e13c0fb9fb601250f95

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
     $(LOCAL_DIR)/omni_oneplus7pro.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_oneplus7pro-user \
+    omni_oneplus7pro-userdebug \
+    omni_oneplus7pro-eng \
