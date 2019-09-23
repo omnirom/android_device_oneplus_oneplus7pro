60068132b2e8d05c5a77828a69380ab36f0560e0
Vachounet <vachounet@live.fr>
2019-09-23 12:43:15 +0200

    oneplus7pro: move brillo_update_payload to host package
    Change-Id: I5058da7c51031f6bfc28a6f482fc566d8e320a6d

--- device.mk
+++ device.mk
@@ -56,10 +56,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
-    brillo_update_payload \
     update_engine \
     update_engine_sideload \
     update_verifier
+
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload
 
 # Boot control
 PRODUCT_PACKAGES_DEBUG += \
