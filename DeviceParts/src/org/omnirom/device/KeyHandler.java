fde6e8a5ed9dbef5bf58e2f3e3852bcb7ab92a51
maxwen <max.weninger@gmail.com>
2019-12-05 11:56:19 +0100

    DeviceParts: panel mode tweaks and fixes
    -remove night mode
    -add wide color mode
    -add info about nned to reboot on off
    -fix selinux denials for srgb
    -use aosp image for panel mode preview
    
    Change-Id: I6209202ba367daa107e8776b8dafc89af8c1c71e

--- DeviceParts/src/org/omnirom/device/KeyHandler.java
+++ DeviceParts/src/org/omnirom/device/KeyHandler.java
@@ -602,34 +602,6 @@ public class KeyHandler implements DeviceKeyHandler {
                 dispatchMediaKeyWithWakeLockToAudioService(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
             }
             return true;
-        } else if (value.equals(AppSelectListPreference.VOLUME_UP_ENTRY)) {
-            OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
-            mAudioManager.adjustSuggestedStreamVolume(AudioManager.ADJUST_RAISE,AudioManager.USE_DEFAULT_STREAM_TYPE,AudioManager.FLAG_SHOW_UI);
-            return true;
-        } else if (value.equals(AppSelectListPreference.VOLUME_DOWN_ENTRY)) {
-            OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
-            mAudioManager.adjustSuggestedStreamVolume(AudioManager.ADJUST_LOWER,AudioManager.USE_DEFAULT_STREAM_TYPE,AudioManager.FLAG_SHOW_UI);
-            return true;
-        } else if (value.equals(AppSelectListPreference.BROWSE_SCROLL_DOWN_ENTRY)) {
-            OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
-            OmniUtils.sendKeycode(KeyEvent.KEYCODE_PAGE_DOWN);
-            return true;
-        } else if (value.equals(AppSelectListPreference.BROWSE_SCROLL_UP_ENTRY)) {
-            OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
-            OmniUtils.sendKeycode(KeyEvent.KEYCODE_PAGE_UP);
-            return true;
-        } else if (value.equals(AppSelectListPreference.NAVIGATE_BACK_ENTRY)) {
-            OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
-            OmniUtils.sendKeycode(KeyEvent.KEYCODE_BACK);
-            return true;
-        } else if (value.equals(AppSelectListPreference.NAVIGATE_HOME_ENTRY)) {
-            OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
-            OmniUtils.sendKeycode(KeyEvent.KEYCODE_HOME);
-            return true;
-        } else if (value.equals(AppSelectListPreference.NAVIGATE_RECENT_ENTRY)) {
-            OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
-            OmniUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
-            return true;
         }
         return false;
     }
