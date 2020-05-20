4cbf237b3f00f94a8fa4bd7a3617ca21df1fb712
mTresk <djtresk@gmail.com>
2020-05-20 15:36:58 +0200

    fod: Reset
    - Switch to op_friginer_print_hbm path (hbm still flickering sometimes)
    - Reset to initial logic (this fixes dc-dimming not working)
    - Fix sRGB path
    - Remove Night Mode
    - Cleanup
    
    Change-Id: I1b2cee587dabe9de63ebd4600f70d4807ae56abd

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -33,17 +33,13 @@
 #define OP_DISPLAY_AOD_MODE 8
 #define OP_DISPLAY_NOTIFY_PRESS 9
 #define OP_DISPLAY_SET_DIM 10
-#define OP_DISPLAY_HIDE_AOD 11
 
-#define HBM_ENABLE_PATH "/sys/class/drm/card0-DSI-1/hbm"
-#define HBM_DIM_PATH "/sys/class/drm/card0-DSI-1/hbm_brightness"
+#define HBM_ENABLE_PATH "/sys/class/drm/card0-DSI-1/op_friginer_print_hbm"
 #define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/dim_alpha"
 #define DC_DIM_PATH "/sys/class/drm/card0-DSI-1/dimlayer_bl_en"
 
 #define NATIVE_DISPLAY_P3 "/sys/class/drm/card0-DSI-1/native_display_p3_mode"
-#define NATIVE_DISPLAY_SRGB "/sys/class/drm/card0-DSI-1/native_display_customer_srgb_mode"
-#define NATIVE_DISPLAY_NIGHT "/sys/class/drm/card0-DSI-1/night_mode"
-
+#define NATIVE_DISPLAY_SRGB "/sys/class/drm/card0-DSI-1/native_display_srgb_color_mode"
 #define NATIVE_DISPLAY_WIDE "/sys/class/drm/card0-DSI-1/native_display_wide_color_mode"
 
 namespace vendor {
@@ -54,8 +50,7 @@ namespace inscreen {
 namespace V1_0 {
 namespace implementation {
 
-int dimAmount;
-int wide,p3,srgb,night;
+int wide,p3,srgb;
 bool dcDimState;
 bool isOneplus7tpro;
 
@@ -100,30 +95,27 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 2);
-
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    set(HBM_ENABLE_PATH, 1);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
-    set(HBM_ENABLE_PATH, 5);
 
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     set(HBM_ENABLE_PATH, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 5);
-    //set(HBM_DIM_PATH, getDimAmount(255));
 
     return Void();
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
-    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
     wide = get(NATIVE_DISPLAY_WIDE, 0);
     p3 = get(NATIVE_DISPLAY_P3, 0);
     srgb = get(NATIVE_DISPLAY_SRGB, 0);
-    night = get(NATIVE_DISPLAY_NIGHT, 0);
 
     this->mVendorDisplayService->setMode(16, 0);
     this->mVendorDisplayService->setMode(17, 0);
@@ -137,19 +129,14 @@ Return<void> FingerprintInscreen::onShowFODView() {
     set(DC_DIM_PATH, 0);
     set(NATIVE_DISPLAY_P3, 0);
     set(NATIVE_DISPLAY_SRGB, 0);
-    set(NATIVE_DISPLAY_NIGHT, 0);
-    set(NATIVE_DISPLAY_WIDE, 1);
-
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 2);
+    set(NATIVE_DISPLAY_WIDE, 0);
 
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
-    set(HBM_ENABLE_PATH, 0);
-    set(DC_DIM_PATH, dcDimState);
-    set(NATIVE_DISPLAY_WIDE, 0);
-
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     this->mVendorDisplayService->setMode(16, 0);
     this->mVendorDisplayService->setMode(17, 0);
     this->mVendorDisplayService->setMode(18, 0);
@@ -158,12 +145,14 @@ Return<void> FingerprintInscreen::onHideFODView() {
     this->mVendorDisplayService->setMode(16, 1);
     this->mVendorDisplayService->setMode(19, 1);
 
+    set(HBM_ENABLE_PATH, 0);
+    set(DC_DIM_PATH, dcDimState);
     set(NATIVE_DISPLAY_WIDE, wide);
     set(NATIVE_DISPLAY_P3, p3);
     set(NATIVE_DISPLAY_SRGB, srgb);
-    set(NATIVE_DISPLAY_NIGHT, night);
 
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
+
     return Void();
 }
 
@@ -205,21 +194,11 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t suggest) {
-    LOG(INFO) << "dimAmount1 = " << suggest;
-    if (suggest > 0) {
-        if (suggest < 127) {
-            dimAmount = 255 - suggest - 16;
-        } else {
-            dimAmount = 92 + get(DIM_AMOUNT_PATH, suggest);
-            if (dimAmount > 237) {
-                dimAmount = 255;
-            }
-        }
-    } else {
-        dimAmount = 180;
-    }
-    LOG(INFO) << "dimAmount2 = " << dimAmount;
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
+    int dimAmount = get(DIM_AMOUNT_PATH, 0);
+
+    LOG(INFO) << "dimAmount = " << dimAmount;
+
     return dimAmount;
 }
 
